9fe5420537fcccaf1c006aec7565fe22907cba20
Sandeep Bisht <>
2024-01-18 15:43:59 +0530

    removed console

--- node.js
+++ node.js
@@ -66,7 +66,6 @@ mongoose.connect(mongoUrl,{
 })
 mongoose.connection.on('connected',()=>{
     console.log('connected to mongo db');
-    console.log('connected to mongo db');
 })
 mongoose.connection.on('error',(err)=>{
     console.log('this is error',err);
